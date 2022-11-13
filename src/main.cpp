af3abb707457205a188eefe372630429a39606da
1lukass <lukaswatkin@gmail.com>
2022-11-12 20:11:08 -0800

    prints a slew of bruhs on the serial monitor

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h> // <<<<< DON'T DELETE THIS!!!
+
+
+/*
+  sends bruh from NAME
+*/
+
+
+void setup() {
+  Serial.begin(115200);
+}
+
+
+void loop() {
+  
+  int space = random() % 50;
+  
+  for (int i = 0; i < space; i++) {
+    Serial.print(" ");
+  }
+
+  Serial.print("bruh\n");
+  delay(10);
+}
